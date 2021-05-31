de9ab48d95b636921632c879bfa48bece007de0c
Ryan <richardryangarcia@gmail.com>
2021-05-31 17:55:38 -0500

    added request and response dtos to token controller

--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -8,15 +8,20 @@ import {
 } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { AuthGuard } from '@nestjs/passport';
+import {
+  LinkTokenResponseDto,
+  AccessTokenResponseDto,
+  AccessTokenRequestDto,
+} from './token.dto';
 
 @Controller('token')
-@UseGuards(AuthGuard())
+// @UseGuards(AuthGuard())
 export class TokenController {
   constructor(private tokenService: TokenService) {}
 
   @Post('/link-token')
   @HttpCode(200)
-  createLinkToken(@Req() req): Promise<void> {
+  createLinkToken(@Req() req): Promise<LinkTokenResponseDto> {
     return this.tokenService.createLinkToken(req.user);
   }
 
@@ -24,8 +29,8 @@ export class TokenController {
   @HttpCode(200)
   exchangeLinkTokenForAccessToken(
     @Req() req,
-    @Body() body: any,
-  ): Promise<void> {
+    @Body() body: AccessTokenRequestDto,
+  ): Promise<AccessTokenResponseDto> {
     const { publicToken } = body;
     return this.tokenService.createAccessToken(req.user, publicToken);
   }
---
+++ src/token/token.dto.ts
@@ -0,0 +1,17 @@
+export type LinkTokenResponseDto = {
+  expiration: string;
+  link_token: string;
+  request_id: string;
+  status_code: number;
+};
+
+export type AccessTokenResponseDto = {
+  access_token: string;
+  item_id: string;
+  request_id: string;
+  status_code: number;
+};
+
+export type AccessTokenRequestDto = {
+  publicToken: string;
+};
